f7c1b1264a773b204655d5e0031d3d91eefa6dd1
Amanpreet kaur <1049amanpreet@gmail.com>
2024-06-06 11:45:12 -1100

    add vercel url

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ db.on("error", (error) => console.error("MongoDB connection error:", error));
 db.once("open", () => console.log("MongoDB connection established"));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin:[ "http://localhost:3000","https://vercel.com/amanpreetheyars-projects/websocket-application"],
 };
 
 app.use(cors(corsOptions));
