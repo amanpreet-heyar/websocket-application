aa3750e8373f489a368234645f67748b5a6dc253
Amanpreet kaur <1049amanpreet@gmail.com>
2024-06-24 12:33:05 -1100

    use another approach for cors error

--- backend/index.js
+++ backend/index.js
@@ -23,12 +23,7 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+
 
 
 const PORT =  5000;
