751f4d4d6fe784a6fc51bbb641994156bef67940
Amanpreet kaur <1049amanpreet@gmail.com>
2024-06-24 13:18:30 -1100

    use fetch instead of axios

--- chat-application/src/components/Auth/Login/index.js
+++ chat-application/src/components/Auth/Login/index.js
@@ -1,60 +1,8 @@
 
-// import { Link, useNavigate } from 'react-router-dom'
-// import './login.css'
-// import { useState } from 'react';
-// import axios from 'axios';
-
-
-// const Login = ({setUser}) => {
-//     const [email, setEmail] = useState();
-//     const [password, setPassword] = useState();
-//     const navigate = useNavigate();
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         axios.post("http://localhost:5000/login", { email, password })
-//             .then(result => {
-//                 console.log(result);
-//                 if (result.data.message === "Success") {
-//                     // console.log(result.data.user, "result data==>")
-//                     setUser(result.data.user)
-//                     navigate("/chat")
-//                 } else {
-//                     navigate("/signup");
-//                     alert("You are not registered to this service");
-//                 }
-//             })
-//             .catch(err => console.log(err));
-//     };
-
-//     return(
-//         <div className='login-container'>
-            
-//             <div className='login-form'>
-//                 <form onSubmit={handleSubmit}>
-//                    <div className='login-form-container'>
-//                        <div><p>Login </p></div>
-//                        <div><input type='email' name='email' placeholder='Enter your E-mail...' value={email} onChange={(e)=>setEmail(e.target.value)}></input></div> 
-//                        <div><input type='password' name='password' placeholder='Enter your password...' value={password} onChange={(e)=> setPassword(e.target.value)}></input></div> 
-//                        <div><button>Log in</button></div>
-//                    </div>
-
-//                     <div >
-//                        <div><p>New User ?</p></div>
-//                        <div><Link to="/signup">Sign Up</Link></div> 
-//                     </div>
-                   
-//                 </form>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Login
 import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
 import { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Login = ({ setUser }) => {
     const [email, setEmail] = useState('');
@@ -63,20 +11,44 @@ const Login = ({ setUser }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // axios.post("http://localhost:5000/login",{email,password})
-        axios.post("https://websocket-application-server-git-main-amanpreetheyars-projects.vercel.app/login", { email, password })
-            .then(result => {
-                console.log(result);
-                if (result.data.message === "Success") {
-                    setUser(result.data.user);
-                    navigate("/chat");
-                } else {
-                    alert("You are not registered to this service");
-                    navigate("/signup");
-                }
-            })
-            .catch(err => console.log(err));
+
+        fetch("https://websocket-application-server-git-main-amanpreetheyars-projects.vercel.app/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            if (result.message === "Success") {
+                setUser(result.user);
+                navigate("/chat");
+            } else {
+                alert("You are not registered to this service");
+                navigate("/signup");
+            }
+        })
+        .catch(err => console.log(err));
     };
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     // axios.post("http://localhost:5000/login",{email,password})
+    //     axios.post("https://websocket-application-server-git-main-amanpreetheyars-projects.vercel.app/login", { email, password })
+    //         .then(result => {
+    //             console.log(result);
+    //             if (result.data.message === "Success") {
+    //                 setUser(result.data.user);
+    //                 navigate("/chat");
+    //             } else {
+    //                 alert("You are not registered to this service");
+    //                 navigate("/signup");
+    //             }
+    //         })
+    //         .catch(err => console.log(err));
+    // };
 
     return (
         <div className='login-container'>
